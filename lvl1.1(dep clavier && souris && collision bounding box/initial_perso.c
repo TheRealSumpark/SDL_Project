e31c7189df492cdb3c87a5dcc3c9d718a2ab4b0c
Speaky98 <47925924+Speaky98@users.noreply.github.com>
2019-03-27 19:40:22 +0100

    Add files via upload

---
+++ lvl1.1(dep clavier && souris && collision bounding box/initial_perso.c
@@ -0,0 +1,12 @@
+#include "initial_perso.h"
+
+Player Initial_personnage()
+{
+Player hero;
+hero.surf=IMG_Load("mob0.png");
+hero.positiontext.x=30;
+hero.positiontext.y=440;
+hero.positiontext.h=hero.surf->h;
+hero.positiontext.w=hero.surf->w;
+return hero;
+}
